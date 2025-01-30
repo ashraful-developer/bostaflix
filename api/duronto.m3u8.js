71ff5f9ddd909619e405c8f11f7e06dd67836830
ashraful-developer <ashrafulislamicchannel@gmail.com>
2025-01-30 09:42:59 +0600

    Update duronto.m3u8.js

--- api/duronto.m3u8.js
+++ api/duronto.m3u8.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
   for (let i = 0; i < 5; i++) {
     const randomNumber = Math.floor(1000 + Math.random() * 9000); // 4-digit random number
     const duration = (Math.random() * 10).toFixed(2); // Random duration
-    segments.push(`#EXTINF:${duration},\n${baseTsUrl}${randomNumber}.ts`);
+    segments.push(`#EXTINF:,\n${baseTsUrl}${randomNumber}.ts`);
   }
 
   const m3u8Content = `#EXTM3U
