5eafcae06c49f17e5c9f7d733114fa2eacce964c
ashraful-developer <ashrafulislamicchannel@gmail.com>
2025-01-31 12:09:08 +0600

    Update stream.m3u8.js

--- api/stream.m3u8.js
+++ api/stream.m3u8.js
@@ -5,7 +5,6 @@ export default async function handler(req, res) {
     2: "https://cdn.hoichoi24.com/gtv-sports-tv5.hoichoi24.com/tracks-v1a1/mono.m3u8",
     3: "https://cdn.hoichoi24.com/t-sports-tv5.hoichoi24.com/tracks-v1a1/mono.m3u8",
     4: "http://stvlive.net:8080/tsports2/tracks-v1a1/mono.m3u8",
-    5: "https://bostaflix.vercel.app/api/stream.m3u8?id=4",
   };
 
   // Define corresponding base URLs
@@ -14,7 +13,6 @@ export default async function handler(req, res) {
     2: "https://cdn.hoichoi24.com/gtv-sports-tv5.hoichoi24.com/tracks-v1a1/",
     3: "https://cdn.hoichoi24.com/t-sports-tv5.hoichoi24.com/tracks-v1a1/",
     4: "http://stvlive.net:8080/tsports2/tracks-v1a1/",
-    5: "https://allinonereborn.com/test.m3u8/ts.php?ts=",
   };
 
   // Get the ID from query parameters
