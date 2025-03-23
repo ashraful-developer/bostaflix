7141c10ee71c3266637c2c8177113328df04d555
ashraful-developer <ashrafulislamicchannel@gmail.com>
2025-03-23 12:34:11 +0600

    Update ythls.m3u8.js

--- api/ythls.m3u8.js
+++ api/ythls.m3u8.js
@@ -33,20 +33,10 @@ function fetchHtml(url) {
     return new Promise((resolve, reject) => {
         const req = request(url, {
             headers: {
-                "authority": "inv.nadeko.net",
-                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                "accept-encoding": "gzip, deflate, br, zstd",
-                "accept-language": "en-US,en;q=0.9,bn;q=0.8",
-                "cache-control": "max-age=0",
-                "cookie": "PREFS=%7B%22volume%22%3A100%2C%22speed%22%3A1%7D; INVIDIOUS_SERVER_ID=2",
-                "dnt": "1",
-                "priority": "u=0, i",
                 "referer": "https://inv.nadeko.net/",
                 "sec-fetch-dest": "document",
                 "sec-fetch-mode": "navigate",
                 "sec-fetch-site": "same-origin",
-                "sec-fetch-user": "?1",
-                "upgrade-insecure-requests": "1",
                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
             },
             timeout: 5000 // 5 seconds timeout
