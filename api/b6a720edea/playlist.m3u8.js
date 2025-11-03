9508fbe975fe5f456f199e6ed8d6780ccc8ecf36
ashraful-developer <ashrafulislamicchannel@gmail.com>
2025-11-03 22:46:04 +0600

    Update playlist.m3u8.js

--- api/b6a720edea/playlist.m3u8.js
+++ api/b6a720edea/playlist.m3u8.js
@@ -2,14 +2,9 @@ export default async function handler(req, res) {
   res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
   res.setHeader("Cache-Control", "no-cache");
 
-  // Generate a random ID for uniqueness (optional)
-  const id = Math.random().toString(36).substring(2, 10);
-
   const playlist = `#EXTM3U
 #EXT-X-VERSION:3
 #EXT-X-TARGETDURATION:6
-#EXT-X-MEDIA-SEQUENCE:0
-#EXT-X-PLAYLIST-TYPE:EVENT
 
 #EXTINF:6.0,
 https://bostaflix.vercel.app/api/video.ts
