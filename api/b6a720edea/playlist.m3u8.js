5d5e1619748ddc60788b28c54c85500fa53368c4
ashraful-developer <ashrafulislamicchannel@gmail.com>
2025-11-04 08:41:35 +0600

    Update playlist.m3u8.js

--- api/b6a720edea/playlist.m3u8.js
+++ api/b6a720edea/playlist.m3u8.js
@@ -1,6 +1,14 @@
 export default async function handler(req, res) {
-  res.writeHead(302, {
-    Location: 'https://livecdn-bostaflix.global.ssl.fastly.net/video.mp4',
-  });
-  res.end();
+  res.setHeader('Content-Type', 'application/vnd.apple.mpegurl');
+  res.setHeader('Cache-Control', 'no-cache');
+
+  const playlist = `#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-TARGETDURATION:8
+#EXT-X-MEDIA-SEQUENCE:0
+#EXTINF:8.0,
+https://bostaflix.vercel.app/video.mp4
+#EXT-X-ENDLIST`;
+
+  res.status(200).send(playlist);
 }
